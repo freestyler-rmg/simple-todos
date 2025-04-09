a86e8938e3b4c75936a89d90f7a965b01fc6cc0b
Roy Margasa <roy.margasa@gmail.com>
2025-04-09 12:58:47 +0700

    added delete logic

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { Ref } from 'vue';
+
+// const inputTaskTitle: Ref<string> = ref('');
+const inputTaskTitle: Ref<string> = defineModel({ default: '' });
+
+const emit = defineEmits(['closeEditor', 'addTask']);
+
+function closeEditor() {
+  emit('closeEditor');
+}
+
+function addTask() {
+  emit('addTask');
+}
+</script>
+
+<template>
+  <input
+    v-model="inputTaskTitle"
+    type="text"
+    class="border-2 border-gray-300 rounded-md p-2 w-full"
+    placeholder="Task name..."
+  />
+  <div class="flex justify-end mt-2">
+    <button class="bg-gray-500 text-white rounded-md py-1 px-3 cursor-pointer" @click="closeEditor">
+      Cancel
+    </button>
+    <button
+      class="bg-blue-500 text-white rounded-md py-1 px-3 cursor-pointer ml-2"
+      @click="addTask"
+    >
+      Add
+    </button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,48 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import type { Ref } from 'vue';
 import { useStorage } from '@vueuse/core';
+import Swal from 'sweetalert2';
 import { TrashIcon, PencilSquareIcon, PlusCircleIcon, Bars2Icon } from '@heroicons/vue/24/solid';
 
-// const tasks = ref([
-//   { id: 1, title: 'Task 1', completed: false },
-//   { id: 2, title: 'Task 2', completed: false },
-// ]);
+import TaskInput from '@/components/TaskInput.vue';
 
-const tasks = useStorage('tasks', []);
+interface task {
+  id: number;
+  title: string;
+  completed: boolean;
+  onEdit: boolean;
+}
 
-const isMainAddTaskInputHidden = ref(true);
-const inputTaskTitle = ref('');
+const SwalStyledButton = Swal.mixin({
+  customClass: {
+    confirmButton: 'bg-red-500 text-white py-2 px-4 rounded-md cursor-pointer mx-2',
+    cancelButton: 'bg-gray-500 text-white py-2 px-4 rounded-md cursor-pointer mx-2',
+  },
+  buttonsStyling: false,
+});
 
-const strikeThroughClass = ref('line-through text-gray-500');
+const tasks: Ref<task[]> = useStorage('tasks', []);
 
-function toggleMainAddTaskInput() {
+// CODE BLOCK - complete a task
+const strikeThroughClass: Ref<string> = ref('line-through text-gray-500');
+
+// CODE BLOCK - add task
+const isMainAddTaskInputHidden: Ref<boolean> = ref(true);
+const inputTaskTitle: Ref<string> = ref('');
+
+function toggleMainAddTaskInput(): void {
   isMainAddTaskInputHidden.value = !isMainAddTaskInputHidden.value;
 }
 
-function addTask() {
+function addTask(): void {
   if (inputTaskTitle.value.trim() === '') return;
 
   const temp = {
     id: tasks.value.length + 1,
     title: inputTaskTitle.value,
     completed: false,
+    onEdit: false,
   };
 
   tasks.value.push(temp);
@@ -33,21 +50,35 @@ function addTask() {
   inputTaskTitle.value = '';
 }
 
-// function deleteTask(id) {
-//   tasks.value = tasks.value.filter(task => task.id !== id);
-// }
+// CODE BLOCK - delete
+const deleteTaskId: Ref<number | null> = ref(null);
 
-// function toggleTaskCompletion(id) {
-//   const task = tasks.value.find(task => task.id === id);
-//   if (task) {
-//     task.completed = !task.completed;
-//   }
-// }
+function triggerDelete(id: number): void {
+  deleteTaskId.value = id;
+
+  SwalStyledButton.fire({
+    text: 'Delete this task?',
+    confirmButtonText: 'Delete',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+    cancelButtonColor: '#d33',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      deleteTask();
+    }
+    deleteTaskId.value = null;
+  });
+}
+
+function deleteTask() {
+  tasks.value = tasks.value.filter((task) => task.id !== deleteTaskId.value);
+}
 </script>
 
 <template>
   <main class="mx-auto max-w-xl py-4 px-2">
     <h1 class="font-bold mb-8 text-4xl">Simple Todos</h1>
+
     <div>
       <div
         v-if="isMainAddTaskInputHidden"
@@ -57,39 +88,39 @@ function addTask() {
         <PlusCircleIcon class="size-6" /><span class="ml-2">Add Task</span>
       </div>
       <div v-else>
-        <input
+        <TaskInput
           v-model="inputTaskTitle"
-          type="text"
-          class="border-2 border-gray-300 rounded-md p-2 w-full"
-          placeholder="Task name..."
+          @close-editor="toggleMainAddTaskInput"
+          @add-task="addTask"
         />
-        <div class="flex justify-end mt-2">
-          <button
-            class="bg-gray-500 text-white rounded-md py-1 px-3 cursor-pointer"
-            @click="toggleMainAddTaskInput"
-          >
-            Cancel
-          </button>
-          <button
-            class="bg-blue-500 text-white rounded-md py-1 px-3 cursor-pointer ml-2"
-            @click="addTask"
-          >
-            Add
-          </button>
-        </div>
       </div>
     </div>
 
     <hr class="my-4" />
 
     <div>
-      <div v-for="task in tasks" class="flex">
-        <div><Bars2Icon class="size-6 text-blue-500" /></div>
-        <div><input type="checkbox" v-model="task.completed" /></div>
-        <div :class="{ [strikeThroughClass]: task.completed }">{{ task.title }}</div>
-        <div class="flex">
-          <div><PencilSquareIcon class="size-4 text-blue-500" /></div>
-          <div class="ml-2"><TrashIcon class="size-4 text-blue-500" /></div>
+      <div v-for="task in tasks" class="flex mb-2" :key="task.id">
+        <div class="p-1 pl-0"><Bars2Icon class="size-6 text-blue-500" /></div>
+        <div class="p-1"><input type="checkbox" v-model="task.completed" /></div>
+        <div class="p-1 flex-auto" :class="{ [strikeThroughClass]: task.completed }">
+          {{ task.title }}
+        </div>
+        <div class="flex p-1">
+          <div>
+            <div
+              class="border border-gray-300 rounded-sm cursor-pointer w-6 h-6 flex items-center justify-center"
+            >
+              <PencilSquareIcon class="size-4 text-blue-500" />
+            </div>
+          </div>
+          <div class="ml-2">
+            <div
+              class="border border-gray-300 rounded-sm cursor-pointer w-6 h-6 flex items-center justify-center"
+              @click="triggerDelete(task.id)"
+            >
+              <TrashIcon class="size-4 text-blue-500" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
